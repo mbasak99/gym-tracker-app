1078dd61d4b388089855282ee1c2dd4d27e5c54d
Monark Basak <monarkb01@gmail.com>
2025-10-22 13:24:42 -0400

    Added Echo lib for dev experience

--- backend/go.mod
+++ backend/go.mod
@@ -6,3 +6,16 @@ require (
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.43.0
 )
+
+require (
+	github.com/labstack/echo/v4 v4.13.4 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/net v0.45.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
+	golang.org/x/text v0.30.0 // indirect
+	golang.org/x/time v0.11.0 // indirect
+)
--- backend/src/main.go
+++ backend/src/main.go
@@ -2,31 +2,39 @@ package main
 
 import (
 	"fmt"
-	"gym-tracker-backend/src/handlers"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World!")
+func home(c echo.Context) error {
+	// fmt.Fprintln(w, "Hello World!")
+	return c.String(http.StatusOK, "Hello World!")
 }
 
 func main() {
-	mux := http.NewServeMux()
+	// mux := http.NewServeMux()
+	mux := echo.New()
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalln("Failed to load .env file")
 	}
 	port := os.Getenv("PORT")
 
+	// Load middlewares
+	mux.Use(middleware.Logger())
+	mux.Use(middleware.Recover())
+
 	// Load handlers
-	mux.HandleFunc("/", home)
-	handlers.RegisterLoginRoutes(mux)
+	mux.GET("/", home)
+	// handlers.RegisterLoginRoutes(mux)
 
 	fmt.Printf("Running on http://localhost%s\n", port)
-	err = http.ListenAndServe(port, mux)
+	// err = http.ListenAndServe(port, mux)
+	err = mux.Start(port)
 	log.Fatalln(err)
 }
