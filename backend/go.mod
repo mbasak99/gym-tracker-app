2f3652ed78f485ee0f228081c0386494004a674f
Monark Basak <monarkb01@gmail.com>
2025-10-21 23:50:29 -0400

    got sign up and login to have some rudementry functionality.

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,7 @@ module gym-tracker-backend
 
 go 1.25.3
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.43.0
+)
--- backend/src/handlers/login.go
+++ backend/src/handlers/login.go
@@ -1,12 +1,66 @@
 package handlers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var hashedPassword []byte
 
 func RegisterLoginRoutes(router *http.ServeMux) {
 	router.HandleFunc("POST /signup", signUp)
-	router.HandleFunc("POST /signin", signIn)
+	router.HandleFunc("POST /login", logIn)
 }
 
-func signUp(w http.ResponseWriter, r *http.Request) { w.Write([]byte("Hello from Sign Up")) }
+func errorHandler(w http.ResponseWriter, output string, err error) {
+	if err != nil {
+		fmt.Fprint(w, output, err)
+	} else {
+		fmt.Fprint(w, output)
+	}
+}
 
-func signIn(w http.ResponseWriter, r *http.Request) { w.Write([]byte("Hello from Sign In")) }
+func logIn(w http.ResponseWriter, r *http.Request) {
+	// grab email and password from req
+	err := r.ParseForm()
+	if err != nil {
+		errorHandler(w, "Failed to parse request body. %+v\n", err)
+		return
+	}
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	// TODO: check email exist in db and convert hashed password in table and compare
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password)) // change hashedPassword to hash from db
+	if err != nil {
+		errorHandler(w, "Invalid credentials. Please try again.\n", nil)
+	}
+
+	fmt.Fprintf(w, "Email: %s, Password: %s, Same?: %t", email, password, err == nil)
+}
+
+func signUp(w http.ResponseWriter, r *http.Request) {
+	// grab email and password from req
+	err := r.ParseForm()
+	if err != nil {
+		errorHandler(w, "Failed to parse request body. %+v\n", err)
+		return
+	}
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	// TODO: check if user already exists
+
+	// hash the password and store it to db
+	hashedPass, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		errorHandler(w, "Failed to hash password. %+v\n", err)
+		return
+	}
+	// hashedPassword = hashedPass
+
+	// w.Write([]byte("Hello from Log In"))
+	fmt.Fprintf(w, "Email: %s, Password: %s, HashedPW: %s", email, password, string(hashedPass))
+}
